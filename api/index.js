6158edc5429a40fa6f10c7b4e029c0c7c38463a5
SViJo0803 <vijo967@gmail.com>
2021-10-19 23:22:49 -0500

    puerto

--- api/index.js
+++ api/index.js
@@ -22,7 +22,8 @@ const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT || 3001, () => {
+  let port_number = server.listen(process.env.PORT || 3001);
+  app.listen(port_number, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
